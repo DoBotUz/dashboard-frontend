a60bdd508a90a09cd99faf441a60ead47cdfd183
unknown <hubzein@gmail.com>
2020-08-13 21:42:44 +0500

    change api routes to bot users

--- src/api/botUsers.js
+++ src/api/botUsers.js
@@ -1,6 +1,6 @@
 import axios from '@/api';
 
 export default () => ({
-  list: (organization) => axios.get(`/bot-users/${organization}/list`),
+  list: (organization) => axios.get(`${organization}/bot-users`),
   update: (payload) => axios.post('/bot-users/update', payload),
 });
