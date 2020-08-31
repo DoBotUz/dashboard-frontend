9dc1e7a65b6ddba6f485e540de5b1e4ab465d8c5
Fakhriddin Abdurakhimov <zealotrahl@gmail.com>
2020-08-31 21:35:26 +0500

    Acl rule type fixed

--- src/acl/index.js
+++ src/acl/index.js
@@ -23,7 +23,7 @@ export default new AclCreate({
   acceptLocalRules : true,
   globalRules: {
     admin  : new AclRule('admin').generate(),
-    operator : new AclRule('operator').or('admin').generate()
+    operator : new AclRule('operator').or('admin').generate(),
     isAdmin: new AclRule(AppRoles.admin).generate(),
     isOwner: new AclRule(AppRoles.owner).or(AppRoles.admin).generate(),
     isManager: new AclRule(AppRoles.manager).or(AppRoles.admin).or(AppRoles.owner).generate(),
